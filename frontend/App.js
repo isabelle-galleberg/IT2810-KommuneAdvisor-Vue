bec68c26aaf7c3cc850bf87fc2e7f544771c10a7
Isabelle Galleberg <isabellegalleberg@gmail.com>
2022-11-07 13:44:42 +0100

    chore: setup Prettier

--- frontend/App.js
+++ frontend/App.js
@@ -4,15 +4,20 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MainPage from './src/pages/MainPage';
 import DetailsPage from './src/pages/DetailsPage';
 
-
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={MainPage} />
-        <Stack.Screen name="Details" component={DetailsPage} />
+        <Stack.Screen
+          name='Home'
+          component={MainPage}
+        />
+        <Stack.Screen
+          name='Details'
+          component={DetailsPage}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
