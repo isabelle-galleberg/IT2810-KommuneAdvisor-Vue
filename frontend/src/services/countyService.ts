c68860e231f2f9aaf068a7313b642e620c2eb604
Isabelle Galleberg <isabellegalleberg@gmail.com>
2022-11-08 11:53:45 +0100

    feat: routing

--- frontend/src/services/countyService.ts
+++ frontend/src/services/countyService.ts
@@ -4,11 +4,13 @@ import { gql } from "@apollo/client/core";
  * Fetch id and name for all counties
  * @returns a list of all counties with id and name
  */
-export const GET_ALL_COUNTIES = gql`
-  query Counties {
-    counties {
-      _id
-      name
+export default function GET_ALL_COUNTIES() {
+  return gql`
+    query Counties {
+      counties {
+        _id
+        name
+      }
     }
-  }
-`;
+  `;
+}
--- frontend/src/views/MainPage.vue
+++ frontend/src/views/MainPage.vue
@@ -1,18 +1,10 @@
 <script>
 import { useQuery } from "@vue/apollo-composable";
-import gql from "graphql-tag";
+import GET_ALL_COUNTIES from "../services/countyService";
 
 export default {
   setup() {
-    const { result, loading, error } = useQuery(gql`
-      query getCounties {
-        counties {
-          _id
-          name
-        }
-      }
-    `);
-
+    const { result, loading, error } = useQuery(GET_ALL_COUNTIES);
     return {
       result,
       loading,
@@ -23,13 +15,23 @@ export default {
 </script>
 
 <template>
-  <div v-if="loading">Loading...</div>
+  <div>
+    <div v-if="loading">Loading...</div>
 
-  <div v-else-if="error">Error: {{ error.message }}</div>
+    <div v-else-if="error">Error: {{ error.message }}</div>
 
-  <ul v-else-if="result && result.counties">
-    <li v-for="user of result.counties" :key="user._id">
-      {{ user.name }}
-    </li>
-  </ul>
+    <ul v-else-if="result && result.counties">
+      <li v-for="user of result.counties" :key="user._id">
+        {{ user.name }}
+      </li>
+    </ul>
+    <el-select v-model="value" class="m-2" placeholder="Select" size="large">
+      <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+      />
+    </el-select>
+  </div>
 </template>
