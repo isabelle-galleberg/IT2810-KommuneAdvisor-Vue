65c01e0af25cd0c1bac8044642e4ada388b7bf60
Isabelle Galleberg <isabellegalleberg@gmail.com>
2022-11-08 14:08:29 +0100

    feat: api requests

--- frontend/src/services/kommuneService.ts
+++ frontend/src/services/kommuneService.ts
@@ -6,31 +6,33 @@ import { gql } from "@apollo/client/core";
  * @returns array with id, name, county, area, population,
  * written language, mapUrl, logoUrl, link to SNL website, average rating and array of ratings
  */
-export const GET_KOMMUNE = gql`
-  query Kommune($id: String) {
-    kommune(id: $id) {
-      _id
-      name
-      county {
+function GET_KOMMUNE() {
+  return gql`
+    query Kommune($id: String) {
+      kommune(id: $id) {
+        _id
         name
-      }
-      snlLink
-      population
-      areaInSquareKm
-      mapUrl
-      logoUrl
-      writtenLanguage
-      averageRating
-      kommuneRating {
-        name
-        rating
-        title
-        description
-        timestamp
+        county {
+          name
+        }
+        snlLink
+        population
+        areaInSquareKm
+        mapUrl
+        logoUrl
+        writtenLanguage
+        averageRating
+        kommuneRating {
+          name
+          rating
+          title
+          description
+          timestamp
+        }
       }
     }
-  }
-`;
+  `;
+}
 
 /**
  * Fetch data for kommuner that fulfill the given criterias
@@ -42,37 +44,39 @@ export const GET_KOMMUNE = gql`
  * @param page page number to fetch
  * @returns array with id, name, county, logoUrl, area, ratings and average rating
  */
-export const GET_ALL_KOMMUNER = gql`
-  query Kommuner(
-    $search: String
-    $sortBy: sort
-    $sortDirection: sortDirection
-    $pageSize: Int
-    $page: Int
-    $county: String
-  ) {
-    kommuner(
-      search: $search
-      sortBy: $sortBy
-      sortDirection: $sortDirection
-      pageSize: $pageSize
-      page: $page
-      county: $county
+function GET_ALL_KOMMUNER() {
+  return gql`
+    query Kommuner(
+      $search: String
+      $sortBy: sort
+      $sortDirection: sortDirection
+      $pageSize: Int
+      $page: Int
+      $county: String
     ) {
-      _id
-      name
-      county {
+      kommuner(
+        search: $search
+        sortBy: $sortBy
+        sortDirection: $sortDirection
+        pageSize: $pageSize
+        page: $page
+        county: $county
+      ) {
+        _id
         name
+        county {
+          name
+        }
+        logoUrl
+        areaInSquareKm
+        kommuneRating {
+          rating
+        }
+        averageRating
       }
-      logoUrl
-      areaInSquareKm
-      kommuneRating {
-        rating
-      }
-      averageRating
     }
-  }
-`;
+  `;
+}
 
 /**
  * Fetch number of kommuner that fulfill the given criterias
@@ -80,8 +84,18 @@ export const GET_ALL_KOMMUNER = gql`
  * @param search search string to filter kommuner by
  * @returns the number of kommuner that fulfill the given criterias
  */
-export const GET_KOMMUNER_COUNT = gql`
-  query kommunerCount($county: String, $search: String) {
-    kommunerCount(search: $search, county: $county)
-  }
-`;
+function GET_KOMMUNER_COUNT() {
+  return gql`
+    query kommunerCount($county: String, $search: String) {
+      kommunerCount(search: $search, county: $county)
+    }
+  `;
+}
+
+const kommuneService = {
+  GET_KOMMUNE,
+  GET_ALL_KOMMUNER,
+  GET_KOMMUNER_COUNT,
+};
+
+export default kommuneService;
--- frontend/src/services/reviewService.ts
+++ frontend/src/services/reviewService.ts
@@ -8,43 +8,54 @@ import { gql } from "@apollo/client";
  * @param title user input for kommune title
  * @param description user input for kommune description
  */
-export const POST_REVIEW = gql`
-  mutation (
-    $name: String!
-    $rating: Int!
-    $title: String!
-    $description: String!
-    $kommuneId: ID!
-  ) {
-    addKommuneRating(
-      name: $name
-      rating: $rating
-      title: $title
-      description: $description
-      kommuneId: $kommuneId
+function POST_REVIEW() {
+  return gql`
+    mutation (
+      $name: String!
+      $rating: Int!
+      $title: String!
+      $description: String!
+      $kommuneId: ID!
     ) {
-      _id
-      timestamp
+      addKommuneRating(
+        name: $name
+        rating: $rating
+        title: $title
+        description: $description
+        kommuneId: $kommuneId
+      ) {
+        _id
+        timestamp
+      }
     }
-  }
-`;
+  `;
+}
 
 /**
  * get all reviews for a kommune
  * @param id id of the kommune to get reviews for
  * @returns array with name, rating, title, description and timestamp
  */
-export const GET_REVIEWS = gql`
-  query Kommune($id: String) {
-    kommune(id: $id) {
-      kommuneRating {
-        name
-        rating
-        title
-        description
-        timestamp
-        _id
+function GET_REVIEWS() {
+  return gql`
+    query Kommune($id: String) {
+      kommune(id: $id) {
+        kommuneRating {
+          name
+          rating
+          title
+          description
+          timestamp
+          _id
+        }
       }
     }
-  }
-`;
+  `;
+}
+
+const reviewService = {
+  POST_REVIEW,
+  GET_REVIEWS,
+};
+
+export default reviewService;
