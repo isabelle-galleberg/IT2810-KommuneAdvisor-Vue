4822cc2324ae7da82ff343adc69643cfdba5f3d5
Ole Marius Inderhaug <ole.inderhaug@visma.com>
2022-11-21 22:34:38 +0100

    fix: move base url to vite config

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import MainPage from "../views/MainPage.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL + "kommuneadvisorvue"),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
